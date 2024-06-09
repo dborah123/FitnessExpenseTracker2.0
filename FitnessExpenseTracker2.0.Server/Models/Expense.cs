389fd463be5c842434703f33e314146a196fd3cb
Dan Borah <danielborah@gmail.com>
2024-06-09 13:54:01 -0500

    Add Expense model validation tags

--- FitnessExpenseTracker2.0.Server/Models/Expense.cs
+++ FitnessExpenseTracker2.0.Server/Models/Expense.cs
@@ -1,6 +1,8 @@
 ﻿
+using Microsoft.IdentityModel.Tokens;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
 using System.Text.Json.Serialization;
 
 namespace FitnessExpenseTracker.Models
@@ -36,40 +38,176 @@ namespace FitnessExpenseTracker.Models
         public string? LinkedActivity { get; set; }
         public SportType? SportsType { get; set; }
 
-        #region Constructors
-        /*
-        public Expense(string? name, double amount, ExpenseType expenseType, DateTime purchaseDate)
+        #region Validation
+        /// <summary>Determines whether the Expense is valid.</summary>
+        /// <param name="errorString">The outputted error string.</param>
+        /// <param name="allowNameGen">if set to <c>true</c> to allow automatic name generation if name is not specified.</param>
+        /// <returns>
+        ///   <c>true</c> if the expense is valid; otherwise, <c>false</c>.</returns>
+        public bool IsExpenseValid(out string errorString, bool allowNameGen = false)
         {
-            this.amount = amount;
-            this.expenseType = expenseType;
-            this.purchaseDate = purchaseDate;
+            StringBuilder errorStringBuilder = new StringBuilder();
 
-            if (name == null)
+            if (!this.IsNameValid())
             {
-                // Set auto-generated name string
-                this.name = GenerateExpenseName(expenseType, purchaseDate);
+                errorStringBuilder.Append(NameInvalid);
+            }
+
+            if (!this.IsStravaUserIDValid())
+            {
+                errorStringBuilder.Append(StravaUserIDInvalid);
+            }
+
+            if (!this.IsAmountValid())
+            {
+                errorStringBuilder.Append(AmountInvalid);
+            }
+
+            if (!this.IsExpenseTypeValid())
+            {
+                errorStringBuilder.Append(ExpenseTypeInvalid);
+            }
+
+            if (!this.IsPurchaseDateValid())
+            {
+                errorStringBuilder.Append(PurchaseDateInvalid);
+            }
+
+            if (!this.IsLinkedActivityValid())
+            {
+                errorStringBuilder.Append(LinkedActivityInvalid);
+            }
+
+            if (!this.IsSportsTypeValid())
+            {
+                errorStringBuilder.Append(SportsTypeInvalid);
+            }
+
+            errorString = errorStringBuilder.ToString();
+
+            return errorString.IsNullOrEmpty();
+        }
+
+        /// <summary>Determines whether the name is valid.</summary>
+        /// <param name="allowNameGen">if set to <c>true to</c> allow automatic name generation if name not specified.</param>
+        /// <returns>
+        ///   <c>true</c> if name valid; otherwise, <c>false</c>.</returns>
+        private bool IsNameValid(bool allowNameGen = false)
+        {
+            if (allowNameGen && this.Name == null)
+            {
+                this.Name = GenExpenseName(this.ExpenseType, this.PurchaseDate);
+            }
+
+            return this.Name is not null;
+        }
+        /// <summary>Determines whether the strava user identifier is valid.</summary>
+        /// <returns>
+        ///   <c>true</c> if strava user identifier valid; otherwise, <c>false</c>.</returns>
+        private bool IsStravaUserIDValid()
+        {
+            return this.StravaUserID is not null;
+        }
+
+        /// <summary>Determines whether the amount is valid.</summary>
+        /// <returns>
+        ///   <c>true</c> if amount is valid; otherwise, <c>false</c>.</returns>
+        private bool IsAmountValid()
+        {
+            if (this.Amount >= 0)
+            {
+                return true;
             }
             else
             {
-                this.name = name;
+                return false;
             }
-
         }
 
-        public Expense(string? name, double amount, ExpenseType expenseType, DateTime purchaseDate, string activity, SportType sportType) 
-            : this(name, amount, expenseType, purchaseDate)
+        /// <summary>Determines whether the expense type is valid.</summary>
+        /// <returns>
+        ///   <c>true</c> if the expense type is valid; otherwise, <c>false</c>.</returns>
+        private bool IsExpenseTypeValid()
         {
-            this.linkedActivity = activity;
-            this.sportsType = sportType;
-        }*/
+
+            if (Enum.IsDefined(typeof(ExpenseType), this.ExpenseType))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>Determines whether the purchase date falls within a valid timeframe.</summary>
+        /// <returns>
+        ///   <c>true</c> if the purchase date is valid; otherwise, <c>false</c>.</returns>
+        private bool IsPurchaseDateValid()
+        {
+            DateTime earliest = new DateTime(2000, 1, 1);
+
+            if (this.PurchaseDate > earliest && this.PurchaseDate <= DateTime.Now)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>Determines whether the linked activity is valid.</summary>
+        /// <returns>
+        ///   <c>true</c> if the linked activity is valid; otherwise, <c>false</c>.</returns>
+        private bool IsLinkedActivityValid()
+        {
+            return true;
+        }
+
+        /// <summary>Determines whether the sports type is valid.</summary>
+        /// <returns>
+        ///   <c>true</c> if the sports type is valid; otherwise, <c>false</c>.</returns>
+        private bool IsSportsTypeValid()
+        {
+            if (this.SportsType == null)
+            {
+                return true;
+            }
+
+            if (Enum.IsDefined(typeof(SportType), this.SportsType))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Validation Error Strings
+        public static readonly string NameInvalid = "Expense name is invalid\n";
+        public static readonly string StravaUserIDInvalid = "Strava user ID is invalid\n";
+        public static readonly string AmountInvalid = "Expense amount must be a greater than 0 value\n";
+        public static readonly string ExpenseTypeInvalid = "Expense type not found\n";
+        public static readonly string PurchaseDateInvalid = "Purchase date must be within the supported timeframe (1/1/2000 - present)\n";
+        public static readonly string LinkedActivityInvalid = "Expense linked activity is invalid\n";
+        public static readonly string SportsTypeInvalid = "Sports type not found\n";
         #endregion
 
         #region Helper functions
-        public void  AutoGenerateExpenseName()
+
+        /// <summary>Auto-generates the expense's name</summary>
+        public void AutoGenerateExpenseName()
         {
-            Name = GenExpenseName(ExpenseType, PurchaseDate);
+            Name = GenExpenseName(this.ExpenseType, this.PurchaseDate);
         }
 
+        /// <summary>Performs generation of the name of the expense.</summary>
+        /// <param name="expenseType">Type of expense.</param>
+        /// <param name="purchaseDate">The purchase date.</param>
+        /// <returns>The expense name.</returns>
         private string GenExpenseName(ExpenseType expenseType, DateTime purchaseDate)
         {
             string expenseTypeName;
